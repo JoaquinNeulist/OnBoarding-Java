4768596fe038ff61e9644d57d74625aa8b2abc03
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-18 16:14:13 -0300

    Vinculacion con git prueba de envio ej1

--- src/dia_1/Main.java
+++ src/dia_1/Main.java
@@ -1,4 +1,12 @@
 package dia_1;
 
 public class Main {
+    public static void main(String[] args) {
+        String myName = "Joaquin";
+        String myLastName = "Neulist";
+        Byte myAge = 18;
+        System.out.printIn("My name is " + myName + " " + myLastName + " and my age is " + myAge);
+
+
+    }
 }
