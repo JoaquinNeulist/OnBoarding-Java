728c3cd43e514ec97287b126e6fd0d8a93ff34cf
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-19 20:16:50 -0300

    Hasta ej 8

--- src/dia_1/Main.java
+++ src/dia_1/Main.java
@@ -1,12 +1,120 @@
 package dia_1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String myName = "Joaquin";
-        String myLastName = "Neulist";
-        Byte myAge = 18;
-        System.out.printIn("My name is " + myName + " " + myLastName + " and my age is " + myAge);
-
-
+        // Ej1
+//        String myName = "Joaquin";
+//        String myLastName = "Neulist";
+//        Byte myAge = 19;
+//        System.out.println("My name is " + myName + " " + myLastName + " and my age is " + myAge);
+        //Ej2
+//        Scanner scanner = new Scanner(System.in);
+//        String welcomeMsg = "Welcome to intellij";
+//        String firstName;
+//        firstName = scanner.next();
+//        System.out.println(welcomeMsg +" "+ firstName);
+        //Ej3
+//        Scanner scanner = new Scanner(System.in);
+//        Integer num1;
+//        Integer num2;
+//        Integer num3;
+//        num1 = scanner.nextInt();
+//        num2 = scanner.nextInt();
+//        num3 = scanner.nextInt();
+//        if (num1 > num2 && num1 > num3){
+//            System.out.println("The largest number is: "+num1);
+//        } else if (num2 > num1 && num2 > num3) {
+//            System.out.println("The largest number is: "+num2);
+//        } else if (num3 > num1 && num3 > num2){
+//            System.out.println("The largest number is: "+num3);
+//        } else {
+//            System.out.println("There are numbers that are equals");
+//        }
+        //ej4
+//        Scanner scanner = new Scanner(System.in);
+//        Integer num;
+//        num = scanner.nextInt();
+//        if (num%2 == 0){
+//            System.out.println("The number "+num+" is even");
+//        }else{
+//            System.out.println("The number "+num+" is odd");
+//        }
+        //ej5
+//            Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a String of characters");
+//        String cadena1 = scanner.nextLine();
+//        System.out.println("Enter a Second string of characters");
+//        String cadena2 = scanner.nextLine();
+//        if (cadena1.contentEquals(cadena2)){
+//            System.out.println("The strings are the same "+cadena1);
+//        }else {
+//            System.out.println("The strings are not the same "+cadena1+" and "+cadena2);
+//        }
+        //ej6
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int num = scanner.nextInt();
+//        boolean isPrime = false;
+//        if (num <= 1) {
+//            isPrime = false;
+//        } else {
+//            isPrime = true;
+//            for (int i = 2; i < num; i++) {
+//                if (num % i == 0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//        }
+//        if (isPrime) {
+//            System.out.println("The number "+num + " is prime.");
+//        } else {
+//            System.out.println("The number "+num + " is not prime.");
+//        }
+        //Ej 7
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter the numbers of elements : ");
+//        int n = scan.nextInt();
+//        int[] numbers = new int[n];
+//        System.out.println("Enter the numbers : ");
+//        for(int i = 0; i < n; i++){
+//            numbers[i] = scan.nextInt();
+//        }
+//        int sumOddNumbers = sumOddNumbers(numbers);
+//        System.out.println("The sum of odd numbers is : "+sumOddNumbers);
+        //8
+//        int[] numbers = {3, 4, 5, 6, 7, 8, 9, 10};
+//        int sumEvenAndPrime = sumEvenAndPrime(numbers);
+//        System.out.println("Sum of even and prime numbers : "+sumEvenAndPrime);
+//    }
+//
+//    public static int sumEvenAndPrime(int[] numbers) {
+//        int sum = 0;
+//        for (int num : numbers) {
+//            if (isEven(num) || isPrime(num)) {
+//                sum += num;
+//            }
+//        }
+//        return sum;
+//    }
+//
+//    public static boolean isEven(int num) {
+//        return num % 2 == 0;
+//    }
+//
+//    public static boolean isPrime(int num) {
+//        if (num <= 1) {
+//            return false;
+//        }
+//        for (int i = 2; i * i <= num; i++) {
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+    //ej9
     }
 }
