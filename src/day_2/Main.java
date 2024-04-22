894ce137a9ffcc7ea7349583d53d108bf3e7112e
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-22 20:50:48 -0300

    correccion de tipado

--- src/day_2/Main.java
+++ src/day_2/Main.java
@@ -1,11 +1,11 @@
 package day_2;
 public class Main {
     public static void main(String[] args) {
-   String[] myPets = {"Mila", "Cory", "Fausto"};
-   Person persona1 = new Person("Joaquin","Neulist","123457869",19, 1.80, false,myPets);
-        persona1.presentCard();
-        String[] myPets2 = {};
-        Person persona2 = new Person("Andres","Ferrari","985746213",20, 1.78, true,myPets2);
-        persona2.presentCard();
+   String[] myPets = {"Mila", "Cory", "Franchesca"};
+   Person me = new Person("Joaquin","Neulist","123457869",19, 1.80, false,myPets);
+        me.presentCard();
+        String[] friendPets = {"Pancho", "Agatha", "Flor"};
+        Person friend = new Person("Andres","Ferrari","985746213",28, 1.78, true,friendPets);
+        friend.presentCard();
     }
 }
