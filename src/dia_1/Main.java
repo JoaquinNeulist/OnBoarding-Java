632ee3e6133950abcbe30bcb6545be6ce1b5730b
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-22 17:52:50 -0300

    Fin day_1

--- src/dia_1/Main.java
+++ src/dia_1/Main.java
@@ -115,8 +115,115 @@ public class Main {
 //        }
 //        return true;
 //    }
-    //ej9
+    //ej9 y 10
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter 2 numbers");
+//        int num1 = scan.nextInt();
+//        int num2 = scan.nextInt();
+//        int choice;
+//        do{
+//            System.out.println("Calculator menu");
+//            System.out.println("1. Add");
+//            System.out.println("2. Subtrat");
+//            System.out.println("3. Multiply");
+//            System.out.println("4. Divide");
+//            System.out.println("0. Exit");
+//            System.out.println("Enter your choice: ");
+//        choice = scan.nextInt();
+//        switch (choice){
+//            case 1:
+//                System.out.println("You chose to add.");
+//                int sum = num1+num2;
+//                System.out.println(num1+"+"+num2+"="+sum);
+//                break;
+//            case 2:
+//                System.out.println("You chose to subtract.");
+//                int subtract = num1-num2;
+//                System.out.println(num1+"-"+num2+"="+subtract);
+//                break;
+//            case 3:
+//                System.out.println("You chose to multiply.");
+//                int product = num1*num2;
+//                System.out.println(num1+"*"+num2+"="+product);
+//                break;
+//            case 4:
+//                if (num2 == 0){
+//                    System.out.println("You cant divide by 0");
+//                }
+//                else{
+//                System.out.println("You chose to divide.");
+//                int division = num1/num2;
+//                System.out.println(num1+"/"+num2+"="+division);
+//                }
+//                break;
+//            case 0:
+//                break;
+//            default:
+//                System.out.println("Invalid choice. Please enter a valid option.");
+//                break;
+//        }
+//        }while (choice != 0);
+    //ej11 y 12
         Scanner scan = new Scanner(System.in);
-
+        int availableCapacity = 500;
+        double moneyRaised = 0.0;
+        int choice;
+        do {
+            System.out.println("Welcome to la bolera.");
+            System.out.println("1. Data entry.");
+            System.out.println("2. Available capacity.");
+            System.out.println("3. Money raised.");
+            System.out.println("4. Log out.");
+            System.out.println("Chose your option:");
+            choice = scan.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Enter your age:");
+                    int age = scan.nextInt();
+                    System.out.println("Enter your id");
+                    String id = scan.next();
+                    System.out.println("Enter your name");
+                    String name = scan.next();
+                    System.out.println("What pass do you have:");
+                    String pass = scan.next();
+                    if (age < 21){
+                        System.out.println("Sorry, only persons over 21 years old are allowed entry.");
+                        return;
+                    }
+                    double ticketPrice = 1500.0;
+                    if (pass.equalsIgnoreCase("VIP")) {
+                        System.out.println("Welcome VIP! Your entry is free.");
+                    } else if (pass.equalsIgnoreCase("Discount")) {
+                        System.out.println("Welcome with discount! Your ticket is half price.");
+                        ticketPrice /= 2;
+                    } else {
+                        System.out.println("Pass type not recognized. Regular ticket price will be charged.");
+                    }
+                        System.out.print("Do you want to buy a regular (R) or VIP (V) ticket? ");
+                        String ticketType = scan.next();
+                        if (ticketType.equalsIgnoreCase("V")) {
+                            ticketPrice = 2000.0; //
+                            System.out.println("Welcome VIP. Thank you for your purchase.");
+                        } else {
+                            System.out.println("Welcome. Thank you for your purchase.");
+                        }
+                        moneyRaised += ticketPrice;
+                        availableCapacity--;
+                        System.out.println("Enjoy your time at the bowling alley.");
+                    break;
+                case 2:
+                    System.out.println("Available capacity: "+availableCapacity);
+                    break;
+                case 3:
+                    System.out.println("Money raised: $"+moneyRaised);
+                    break;
+                case 4:
+                    System.out.println("You_log_out.");
+                    break;
+                default:
+                    System.out.println("Jimmy option"); //South park reference
+                    break;
+            }
+        }while (choice!=4);
     }
 }
